38b43b54ff786b066a28de6d597bef019285fbda
shubhi <support@appice.io>
2019-08-27 13:13:25 +0530

    added rest component

--- src/app/component/home/home-routing.module.ts
+++ src/app/component/home/home-routing.module.ts
@@ -5,7 +5,17 @@ import { MainComponent } from './main/main.component';
 import { CategoryV1Component } from './category-v1/category-v1.component';
 import { CategoryV12Component } from './category-v12/category-v12.component';
 import { ProductV1Component } from './product-v1/product-v1.component';
-
+import { CartpageComponent } from './cartpage/cartpage.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CompareComponent } from './compare/compare.component';
+import { QuickviewComponent } from './quickview/quickview.component';
+import { TrackorderComponent } from './trackorder/trackorder.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Error404Component } from './error404/error404.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
 
 const routes: Routes = [
   {
@@ -27,6 +37,50 @@ const routes: Routes = [
       {
         path : 'product_v1',
         component : ProductV1Component   
+      },
+      {
+        path : 'cartpage',
+        component : CartpageComponent   
+      },
+      {
+        path : 'checkout',
+        component : CheckoutComponent   
+      },
+      {
+        path : 'compare',
+        component : CompareComponent   
+      },
+      {
+        path : 'quickview',
+        component : QuickviewComponent   
+      },
+      {
+        path : 'trackyourorder',
+        component : TrackorderComponent   
+      },
+      {
+        path : 'wishlist',
+        component : WishlistComponent   
+      },
+      {
+        path : 'about',
+        component : AboutComponent   
+      },
+      {
+        path : 'contact',
+        component : ContactComponent   
+      },
+      {
+        path : '404',
+        component : Error404Component   
+      },
+      {
+        path : 'blog',
+        component : BlogComponent   
+      },
+      {
+        path : 'blogdetail',
+        component : BlogdetailsComponent   
       }
     ]
   }
--- src/app/component/home/home.module.ts
+++ src/app/component/home/home.module.ts
@@ -11,10 +11,21 @@ import { OwlModule } from 'ngx-owl-carousel';
 import { CategoryV1Component } from './category-v1/category-v1.component';
 import { CategoryV12Component } from './category-v12/category-v12.component';
 import { ProductV1Component } from './product-v1/product-v1.component';
+import { CartpageComponent } from './cartpage/cartpage.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CompareComponent } from './compare/compare.component';
+import { QuickviewComponent } from './quickview/quickview.component';
+import { TrackorderComponent } from './trackorder/trackorder.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Error404Component } from './error404/error404.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, FooterComponent, SidebarComponent, MainComponent, CategoryV1Component, CategoryV12Component, ProductV1Component],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent, SidebarComponent, MainComponent, CategoryV1Component, CategoryV12Component, ProductV1Component, CartpageComponent, CheckoutComponent, CompareComponent, QuickviewComponent, TrackorderComponent, WishlistComponent, AboutComponent, ContactComponent, Error404Component, BlogComponent, BlogdetailsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule, 
